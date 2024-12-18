7a5c0e39fa7e5e4a6defe8560e29488177974f46
Candy <dltkr0424@gmail.com>
2024-12-18 10:43:57 +0900

    Feat : Skill

---
+++ Assets/00.Work/ISC/01.Scirpts/Manager/SkillManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+
+public enum SkillType
+{
+    Reload,
+    Haste,
+    TriggerStrength,
+    Adrenaline,
+    Penetration,
+    ElectronicBullet,
+    BoomIsArt,
+    LookLikeLong,
+    LimeBullet,
+    CellBullet,
+    Seasoneded,
+    BloodedBullet,
+    CellPet,
+    BoomPet,
+    IcePet
+}
+public class SkillManager : MonoBehaviour
+{
+}
