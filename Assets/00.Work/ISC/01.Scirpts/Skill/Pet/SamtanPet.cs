2fd967be0588519323d48eccea4ed2c2d8a5c515
Candy <dltkr0424@gmail.com>
2024-12-18 19:55:08 +0900

    Feat : SamtanPet

--- Assets/00.Work/ISC/01.Scirpts/Skill/Pet/SamtanPet.cs
+++ Assets/00.Work/ISC/01.Scirpts/Skill/Pet/SamtanPet.cs
@@ -1,13 +1,19 @@
+using GGMPool;
 using UnityEngine;
 
-public class SantanPet : Pet
+public class SamtanPet : Pet
 {
+    [SerializeField] private PoolManagerSO poolManagerSO;
+    [SerializeField] private PoolTypeSO poolTypeSO;
+    
     private Vector3 _dir;
     protected override void Attack(GameObject target)
     {
         _dir = target.transform.position - transform.position;
         float rotate = Mathf.Atan2(_dir.y, _dir.x) * Mathf.Rad2Deg;
-        
+
         transform.localRotation = Quaternion.Euler(0, 0, rotate);
+        PetBullet obj = poolManagerSO.Pop(poolTypeSO) as PetBullet;
+        
     }
 }
