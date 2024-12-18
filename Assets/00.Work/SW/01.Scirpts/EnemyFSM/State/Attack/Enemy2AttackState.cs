9769eedda677ef7b704e8a9563bebdeb80daffc4
pig-08 <157554537+pig-08@users.noreply.github.com>
2024-12-18 16:36:27 +0900

    Feat : Enemy1 Attack

---
+++ Assets/00.Work/SW/01.Scirpts/EnemyFSM/State/Attack/Enemy2AttackState.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using UnityEngine;
+
+public class Enemy2AttackState : EnmyState
+{
+    private MoveDecision moveDecision;
+    public override void OnEnterState()
+    {
+        if (moveDecision == null)
+            foreach (EnemyDecision decision in _decisions)
+                if (decision.GetComponent<MoveDecision>() != null) moveDecision = decision.GetComponent<MoveDecision>();
+
+        moveDecision.IsMoveEnd = false;
+        _brain.EnemyRIgid.linearVelocity = Vector3.zero;
+        StartCoroutine(StartAttackTime());
+    }
+
+    private IEnumerator StartAttackTime()
+    {
+        yield return new WaitForSeconds(0.2f);
+        moveDecision.IsMoveEnd = true;
+    }
+
+    public override void OnExitState()
+    {
+
+    }
+
+    public override void UpdateState()
+    {
+        base.UpdateState();
+    }
+}
