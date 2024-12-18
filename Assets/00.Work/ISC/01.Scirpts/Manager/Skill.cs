2737419c46b2d0ee933be3280440658c9a0447f9
Candy <dltkr0424@gmail.com>
2024-12-18 15:59:41 +0900

    Fix : Skill

--- Assets/00.Work/ISC/01.Scirpts/Manager/Skill.cs
+++ Assets/00.Work/ISC/01.Scirpts/Manager/Skill.cs
@@ -1,16 +1,11 @@
 using System;
 using UnityEngine;
 
-public delegate void CooldownInfoEvent(float cureent, float total);
 public abstract class Skill : MonoBehaviour
 {
     public bool skillEnabled = false;
-    [SerializeField] protected float cooldown;
-    protected float CooldownTimer;
+    
     protected Player Player;
-
-    public bool IsCooldown => CooldownTimer > 0f;
-    public event CooldownInfoEvent OnCooldownInfoEvent;
 
     public virtual void Initialize(Player player)
     {
@@ -20,33 +15,6 @@ public abstract class Skill : MonoBehaviour
     public void OnSkill()
     {
         skillEnabled = true;
-    }
-
-    protected virtual void Update()
-    {
-        if (CooldownTimer > 0)
-        {
-            CooldownTimer -= Time.deltaTime;
-            
-            if (CooldownTimer <= 0)
-            {
-                CooldownTimer = 0;
-            }
-            
-            OnCooldownInfoEvent?.Invoke(CooldownTimer, cooldown);
-        }
-    }
-
-    public virtual bool AttemptUseSkill()
-    {
-        if (CooldownTimer <= 0 && skillEnabled)
-        {
-            CooldownTimer = cooldown;
-            UseSkill();
-            return true;
-        }
-        Debug.Log("Skill cooldown or locked");
-        return false;
     }
 
     public virtual void UseSkill()
