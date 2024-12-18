16f04613d3fa4a45476a9efb2f3c03153b63a430
Candy <dltkr0424@gmail.com>
2024-12-18 18:07:21 +0900

    Feat : SantanPet

--- Assets/00.Work/ISC/01.Scirpts/Skill/Pet/SantanPet.cs
+++ Assets/00.Work/ISC/01.Scirpts/Skill/Pet/SantanPet.cs
@@ -2,8 +2,12 @@ using UnityEngine;
 
 public class SantanPet : Pet
 {
+    private Vector3 _dir;
     protected override void Attack(GameObject target)
     {
+        _dir = target.transform.position - transform.position;
+        float rotate = Mathf.Atan2(_dir.y, _dir.x) * Mathf.Rad2Deg;
         
+        transform.localRotation = Quaternion.Euler(0, 0, rotate);
     }
 }
