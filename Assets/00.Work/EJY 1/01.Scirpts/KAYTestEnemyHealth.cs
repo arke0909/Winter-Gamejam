21e798461a04ccdc968c3e64e41bfacc5b3db406
gdhsf3701 <gdhsf3701@gmail.com>
2024-12-19 12:17:56 +0900

    remove : TestHealthCode

--- Assets/00.Work/EJY 1/01.Scirpts/KAYTestEnemyHealth.cs
+++ Assets/00.Work/EJY 1/01.Scirpts/KAYTestEnemyHealth.cs
@@ -3,18 +3,7 @@ using UnityEngine;
 
 public class KAYTestEnemyHealth :Health
 {
-    int max = 100;
-
-    protected override void Die()
-    {
-        base.Die();
-        Debug.Log("die");
-    }
-
-    private void Start()
-    {
-        Initialize(max);    
-    }
+    
 
 
 }
