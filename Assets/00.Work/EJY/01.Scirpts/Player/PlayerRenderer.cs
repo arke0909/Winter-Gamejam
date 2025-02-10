ded4ad5652d714fff0370142a45cd581e3a69c3f
pig-08 <157554537+pig-08@users.noreply.github.com>
2025-02-10 21:54:08 +0900

    Faet : Stage

--- Assets/00.Work/EJY/01.Scirpts/Player/PlayerRenderer.cs
+++ Assets/00.Work/EJY/01.Scirpts/Player/PlayerRenderer.cs
@@ -4,6 +4,7 @@ public class PlayerRenderer : MonoBehaviour, IPlayerComponent
 {
     private Player _player;
     private InputReader _inputReader;
+    [SerializeField] private Transform _tm;
 
     public void Initialize(Player player)
     {
@@ -20,7 +21,7 @@ public class PlayerRenderer : MonoBehaviour, IPlayerComponent
 
     private void RotateAndFlip()
     {
-        Vector2 mousePos = _inputReader.MousePos;
+        Vector2 mousePos = new Vector2( _inputReader.MousePos.x - _tm.transform.position.x , _inputReader.MousePos.y - _tm.transform.position.y);
         float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
 
         transform.parent.rotation = Quaternion.Euler(0, 0, angle);
