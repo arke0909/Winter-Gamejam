5e3b51cf1258ceb1d9cee0584f340f09777d9fd4
gdhsf3701 <gdhsf3701@gmail.com>
2025-02-16 21:19:40 +0900

    Fixed : Slider

--- Assets/00.Work/KAY/UISetting.cs
+++ Assets/00.Work/KAY/UISetting.cs
@@ -2,10 +2,29 @@ using UnityEngine;
 
 public class UISetting : MonoSingleton<UISetting>
 {
-    
+    [SerializeField] GameObject settingUI;
+
     private void Awake()
     {
         DontDestroyOnLoad(this);
 
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(settingUI.activeSelf)
+            {
+                settingUI.SetActive(false);
+                
+
+            }
+            else
+            {
+                settingUI.SetActive(true);
+                
+            }
+        }
     }
 }
